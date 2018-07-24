416550dfbfe588c6713c2ecfb4ef4dd9a7eddb6d
jeffcrowley <jeffcrowley@Jeffs-MacBook-Pro.local>
2018-07-23 18:11:04 -0700

    finish lesson 14

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Header from './Header'
+import Order from './Order'
+import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+  addFish = (fish) => {
+    // 1. take a copy of existing state
+    const fishes = {...this.state.fishes}
+    // 2. add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish
+    // 3. set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    })
+  }
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" age={500} cool={true} />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+      </div>
+    )
+  }
+}
+
+export default App
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef()
+
+  goToStore = (e) => {
+    // 1. Stop the form from submitting
+    e.preventDefault()
+    // 2.get the text from that input
+    const storeName = this.myInput.value.value
+    // 3. change the page to /store/whatever
+    this.props.history.push(`/store/${storeName}`)
+  }
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref ={this.myInput} required="true" placeholder="Store Name" defaultValue={ getFunName() } />
         <button type="submit">Visit Store -></button>
       </form>
     )
